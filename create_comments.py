e7df7a9b59a7d8a4471316528f6968bdd82f1798
libredesarrollo <desarrollolibre.net@gmail.com>
2024-01-05 06:24:33 -0400

    CRUD comentarios

--- create_comments.py
+++ create_comments.py
@@ -1,26 +1,24 @@
 from faker import Faker
 
-from comments.models import Comment
-
 def main():
     fake = Faker()
 
     for _ in range(5):
-        comment = Comment.object.create(
+        comment = Comment.objects.create(
             text = fake.text()
         )
 
     comments = Comment.objects.all()
 
-    print(f'Comments in DB {comments.count()}')
+    print(f"Comments in DB {comments.count()}")
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     import os
     from django.core.wsgi import get_wsgi_application
 
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mystore.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE','mystore.settings')
+    aplication = get_wsgi_application()
 
-    application = get_wsgi_application()
+    from comments.models import Comment
 
     main()
